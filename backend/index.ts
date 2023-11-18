aa64def4803d479431426a9cc3799ede71f4c5c8
pbj0922 <qkrqjawls0922@gmail.com>
2023-11-18 10:11:56 +0900

    prisma setting

--- backend/index.ts
+++ backend/index.ts
@@ -1,7 +1,17 @@
 import express, { Express } from "express";
+import cors from "cors";
 
 const app: Express = express();
 const port: number = +process.env.PORT! || 3010;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  cors({
+    origin: process.env.FRONT_URL,
+    credentials: true,
+  })
+);
 
 app.get("/", (req, res) => {
   res.send("Hello, ExpressTS!");
